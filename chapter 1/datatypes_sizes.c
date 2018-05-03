b6058ddcee066c306b2dd9e45d1766aea59ea4de
sameeranjoshi <joshisameeran17@gmail.com>
2018-05-03 22:38:05 +0530

    Add files via upload

---
+++ chapter 1/datatypes_sizes.c
@@ -0,0 +1,18 @@
+/*
+author:sameeran joshi <joshisameeran17@gmail.com>
+date:3-may-2018
+program to check the sizeof operator and check the sizes of datatypes
+*/
+
+#include<stdio.h>
+#include<limits.h>
+int main(){
+printf("size is %d",sizeof(int));
+printf("size is %d",sizeof(short int));
+printf("size is %d",sizeof(long int));
+printf("size is %d",sizeof(signed int));
+printf("size is %d",sizeof(unsigned int));
+printf("size is %d",sizeof(float));
+printf("size is %d",sizeof(char));
+
+}
